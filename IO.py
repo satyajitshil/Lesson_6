ad09434f60504ac8efdf7c27436f469cce8148e8
Satyajit Shil <satyajitshil02@gmail.com>
2025-06-13 16:45:12 -0700

    first commit

---
+++ IO.py
@@ -0,0 +1,8 @@
+a = 22
+b = 23
+c = 24
+
+if b > a or b < c:
+    print("I am using or")
+elif a > b and a < c:
+    print("I am using and")
---
+++ inequality.py
@@ -0,0 +1,17 @@
+a = 10
+b = 11
+c = 11
+print(a != b)
+print(b != c)
+
+a = "coding"
+b = "python"
+
+if a != b:
+    print(a, "and",b, "are not equal")
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+    print(bool())
